0cc3fd472fefb11f79fddab9eb18ad012a3f655c
R3call <thenoobyzombie@gmail.com>
2024-02-25 20:16:20 +0100

    updated: new landing page components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,19 @@ import MaxWidthWrapper from "@/components/maxwidthwrapper";
 import Hero from "@/components/hero";
 import Features from "@/components/features";
 import Video from "@/components/video";
+import Benefit from "@/components/benefits";
+import CTA from "@/components/cta";
+
 
 export default function Home() {
-  // Now working on Video section
+	// Now working on Video section
 	return (
 		<MaxWidthWrapper>
 			<Hero />
 			<Video />
-      <Features />
+			<Features />
+			<Benefit />
+			<CTA />
 		</MaxWidthWrapper>
 	);
 }
